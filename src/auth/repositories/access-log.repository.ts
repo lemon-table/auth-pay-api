fb514dafde1f66afe412908b14997840296ccac0
=lemon-table <=>
2024-03-21 23:20:40 +0900

    refactor: 코드 탭 정리

--- src/auth/repositories/access-log.repository.ts
+++ src/auth/repositories/access-log.repository.ts
@@ -5,22 +5,22 @@ import { AccessLog, User } from '../entities';
 
 @Injectable()
 export class AccessLogRepository extends Repository<AccessLog> {
-  constructor(
-    @InjectRepository(AccessLog)
-    private readonly repo: Repository<AccessLog>,
-    @InjectEntityManager()
-    private readonly entityManager: EntityManager,
-  ) {
-    super(repo.target, repo.manager, repo.queryRunner);
-  }
+    constructor(
+        @InjectRepository(AccessLog)
+        private readonly repo: Repository<AccessLog>,
+        @InjectEntityManager()
+        private readonly entityManager: EntityManager,
+    ) {
+        super(repo.target, repo.manager, repo.queryRunner);
+    }
 
-  async createAccessLog(user: User, ua: string, endpoint: string, ip: string) {
-    const accessLog = new AccessLog();
-    accessLog.user = user;
-    accessLog.ua = ua;
-    accessLog.endpoint = endpoint;
-    accessLog.ip = ip;
-    accessLog.accessedAt = new Date(Date.now());
-    await this.save(accessLog);
-  }
+    async createAccessLog(user: User, ua: string, endpoint: string, ip: string) {
+        const accessLog = new AccessLog();
+        accessLog.user = user;
+        accessLog.ua = ua;
+        accessLog.endpoint = endpoint;
+        accessLog.ip = ip;
+        accessLog.accessedAt = new Date(Date.now());
+        await this.save(accessLog);
+    }
 }
--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -91,12 +91,9 @@ export class AuthService {
 
     async refreshAccessToken(refreshToken: string): Promise<string> {
         try {
-            const { exp, ...payload } = await this.jwtService.verifyAsync(
-                refreshToken,
-                {
-                  secret: this.configService.get<string>('JWT_SECRET'),
-                },
-          );
+            const { exp , ...payload } = await this.jwtService.verifyAsync(refreshToken, {
+                secret: this.configService.get<string>('JWT_SECRET'),
+            });
 
             const user = await this.userRepository.findOneBy({ id: payload.sub });
             if (!user) {
@@ -110,110 +107,90 @@ export class AuthService {
 
             return this.createAccessToken(user, payload as TokenPayload);
         } catch (error) {
-          throw new BusinessException(
-            'auth',
-            'invalid-refresh-token',
-            'Invalid refresh token',
-            HttpStatus.UNAUTHORIZED,
-          );
+            throw new BusinessException(
+                'auth',
+                'invalid-refresh-token',
+                'Invalid refresh token',
+                HttpStatus.UNAUTHORIZED,
+            );
         }
     }
 
-  createTokenPayload(userId: string): TokenPayload {
-    return {
-      sub: userId,
-      iat: Math.floor(Date.now() / 1000),
-      jti: uuidv4(),
-    };
-  }
-
-  async createAccessToken(user: User, payload: TokenPayload): Promise<string> {
-    const expiresIn = this.configService.get<string>('ACCESS_TOKEN_EXPIRY');
-    const token = this.jwtService.sign(payload, { expiresIn });
-    const expiresAt = this.calculateExpiry(expiresIn);
-
-    await this.accessTokenRepository.saveAccessToken(
-      payload.jti,
-      user,
-      token,
-      expiresAt,
-    );
-
-    return token;
-  }
-
-  async createRefreshToken(user: User, payload: TokenPayload): Promise<string> {
-    const expiresIn = this.configService.get<string>('REFRESH_TOKEN_EXPIRY');
-    const token = this.jwtService.sign(payload, { expiresIn });
-    const expiresAt = this.calculateExpiry(expiresIn);
-
-    await this.refreshTokenRepository.saveRefreshToken(
-      payload.jti,
-      user,
-      token,
-      expiresAt,
-    );
-
-    return token;
-  }
-
-  private async validateUser(
-    email: string,
-    plainPassword: string,
-  ): Promise<User> {
-    const user = await this.userRepository.findOne({ where: { email } });
-    if (user && (await argon2.verify(user.password, plainPassword))) {
-      return user;
-    }
-    throw new BusinessException(
-      'auth',
-      'invalid-credentials',
-      'Invalid credentials',
-      HttpStatus.UNAUTHORIZED,
-    );
-  }
-
-  private async addToBlacklist(
-    token: string,
-    jti: string,
-    type: 'access' | 'refresh',
-    expiryConfigKey: string,
-  ): Promise<void> {
-    const expiryTime = this.calculateExpiry(
-      this.configService.get<string>(expiryConfigKey),
-    );
-    await this.tokenBlacklistService.addToBlacklist(
-      token,
-      jti,
-      type,
-      expiryTime,
-    );
-  }
-
-  private calculateExpiry(expiry: string): Date {
-    let expiresInMilliseconds = 0;
-
-    if (expiry.endsWith('d')) {
-      const days = parseInt(expiry.slice(0, -1), 10);
-      expiresInMilliseconds = days * 24 * 60 * 60 * 1000;
-    } else if (expiry.endsWith('h')) {
-      const hours = parseInt(expiry.slice(0, -1), 10);
-      expiresInMilliseconds = hours * 60 * 60 * 1000;
-    } else if (expiry.endsWith('m')) {
-      const minutes = parseInt(expiry.slice(0, -1), 10);
-      expiresInMilliseconds = minutes * 60 * 1000;
-    } else if (expiry.endsWith('s')) {
-      const seconds = parseInt(expiry.slice(0, -1), 10);
-      expiresInMilliseconds = seconds * 1000;
-    } else {
-      throw new BusinessException(
-        'auth',
-        'invalid-expiry',
-        'Invalid expiry time',
-        HttpStatus.BAD_REQUEST,
-      );
+    createTokenPayload(userId: string): TokenPayload {
+        return {
+            sub: userId,
+            iat: Math.floor(Date.now() / 1000),
+            jti: uuidv4(),
+        };
     }
 
-    return new Date(Date.now() + expiresInMilliseconds);
-  }
+    async createAccessToken(user: User, payload: TokenPayload): Promise<string> {
+        const expiresIn = this.configService.get<string>('ACCESS_TOKEN_EXPIRY');
+        const token = this.jwtService.sign(payload, { expiresIn });
+        const expiresAt = this.calculateExpiry(expiresIn);
+
+        await this.accessTokenRepository.saveAccessToken(payload.jti, user, token, expiresAt);
+
+        return token;
+    }
+
+    async createRefreshToken(user: User, payload: TokenPayload): Promise<string> {
+        const expiresIn = this.configService.get<string>('REFRESH_TOKEN_EXPIRY');
+        const token = this.jwtService.sign(payload, { expiresIn });
+        const expiresAt = this.calculateExpiry(expiresIn);
+
+        await this.refreshTokenRepository.saveRefreshToken(payload.jti, user, token, expiresAt);
+
+        return token;
+    }
+
+    private async validateUser(email: string, plainPassword: string): Promise<User> {
+        const user = await this.userRepository.findOne({ where: { email } });
+        if (user && (await argon2.verify(user.password, plainPassword))) {
+            return user;
+        }
+        throw new BusinessException(
+            'auth',
+            'invalid-credentials',
+            'Invalid credentials',
+            HttpStatus.UNAUTHORIZED,
+        );
+    }
+
+    private async addToBlacklist(
+        token: string,
+        jti: string,
+        type: 'access' | 'refresh',
+        expiryConfigKey: string,
+    ): Promise<void> {
+        const expiryTime = this.calculateExpiry(this.configService.get<string>(expiryConfigKey));
+        await this.tokenBlacklistService.addToBlacklist(token, jti, type, expiryTime);
+    }
+
+    private calculateExpiry(expiry: string): Date {
+        let expiresInMilliseconds = 0;
+
+        if (expiry.endsWith('d')) {
+            const days = parseInt(expiry.slice(0, -1), 10);
+            expiresInMilliseconds = days * 24 * 60 * 60 * 1000;
+        } else if (expiry.endsWith('h')) {
+            const hours = parseInt(expiry.slice(0, -1), 10);
+            expiresInMilliseconds = hours * 60 * 60 * 1000;
+        } else if (expiry.endsWith('m')) {
+            const minutes = parseInt(expiry.slice(0, -1), 10);
+            expiresInMilliseconds = minutes * 60 * 1000;
+        } else if (expiry.endsWith('s')) {
+            const seconds = parseInt(expiry.slice(0, -1), 10);
+            expiresInMilliseconds = seconds * 1000;
+        } else {
+            throw new BusinessException(
+                'auth',
+                'invalid-expiry',
+                'Invalid expiry time',
+                HttpStatus.BAD_REQUEST,
+            );
+        }
+
+        return new Date(Date.now() + expiresInMilliseconds);
+    }
 }
